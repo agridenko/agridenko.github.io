b139322cc96db452aa8fc7b51d59df8c1ec570d6
gridenko <gridenko@digitalwill.ru>
2018-08-24 13:08:57 +0300

    second commit -> add all files

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+	<ul>
+		<li>some text</li>
+		<li>some text1</li>
+		<li>some text2</li>
+	</ul>
+</footer>
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@include('header');
+
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Alexey Gridenko
+                </div>
+                <div class="work-title">
+                    Web developer
+                </div>
+
+                {{--<div class="links">--}}
+                    {{--<a href="https://laravel.com/docs">Documentation</a>--}}
+                    {{--<a href="https://laracasts.com">Laracasts</a>--}}
+                    {{--<a href="https://laravel-news.com">News</a>--}}
+                    {{--<a href="https://forge.laravel.com">Forge</a>--}}
+                    {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
+                    {{--<a href="/my-work/">My work!!!</a>--}}
+                {{--</div>--}}
+            </div>
+
+
+
+        </div>
+
+@include('footer');
